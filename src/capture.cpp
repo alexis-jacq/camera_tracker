c90774d80e9fa6a6991be5867cdc74edb9900470
Alexis Jacq <alexis.jacq@epfl.ch>
2014-10-15 15:12:50 +0200

    not tracked face detection displayed with ellipse

--- src/capture.cpp
+++ src/capture.cpp
@@ -1,8 +1,17 @@
 #include "opencv2/opencv.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 
 using namespace cv;
 using namespace std;
+
+/** Global variables */
+String face_cascade_name = "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml";
+String eyes_cascade_name = "/usr/share/opencv/haarcascades/haarcascade_eye_tree_eyeglasses.xml";
+CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
 
 int main(int, char**)
 {
@@ -12,6 +21,10 @@ int main(int, char**)
     {
         return -1;
     }
+
+    //-- 1. Load the cascades
+    if( !face_cascade.load( face_cascade_name ) ){ cout << "--(!)Error loading"<< std::endl ; return -1; };
+    if( !eyes_cascade.load( eyes_cascade_name ) ){ cout << "--(!)Error loading"<< std::endl ; return -1; };
 
     // init features with goodfeaturtotrack
     //-------------------------------------
@@ -29,6 +42,7 @@ int main(int, char**)
     Mat next_gframe;
     vector<Point2f> next_pts;
     Vec3b color;
+    std::vector<Rect> faces;
 
     while(true)
     {
@@ -47,6 +61,18 @@ int main(int, char**)
             color[3]=0;
             frame.at<Vec3b>(next_pts[i]) = color; // set color
         }
+
+        // face cascade detection
+        //-----------------------
+
+        // detect + display faces and eyes
+        face_cascade.detectMultiScale( next_gframe, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+        for( size_t i = 0; i < faces.size(); i++ )
+        {
+            Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
+            ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+         }
+
 
         imshow("tracking", frame);
 
