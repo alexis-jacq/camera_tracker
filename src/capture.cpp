85dcc7c5830ef828a5c3408b83359b4d8cb4d3c4
Alexis Jacq <alexis.jacq@epfl.ch>
2014-10-15 13:36:27 +0200

    tracking features with calcOptimalFlow

--- src/capture.cpp
+++ src/capture.cpp
@@ -13,25 +13,47 @@ int main(int, char**)
         return -1;
     }
 
+    // init features with goodfeaturtotrack
+    //-------------------------------------
+    Mat frame;
     Mat gframe; // gray frame that will be used for goodfeaturtotrack
+    cap >> frame; // get a new frame from camera
+    cvtColor(frame, gframe, CV_BGR2GRAY); // color -> gray
+    vector<Point2f> pts; // 2D vector for the list of corner points
+    goodFeaturesToTrack(gframe,pts,30,0.01,10,noArray(),3,false,0.04); // get the good corners
 
-    namedWindow("edges",1);
+    // loop with tracking with calcOptimalFlow
+    //----------------------------------------
+    namedWindow("tracking",1);
+
+    Mat next_gframe;
+    vector<Point2f> next_pts;
+    Vec3b color;
 
     while(true)
     {
-        Mat frame;
+        cap >> frame;
+        cvtColor(frame, next_gframe, CV_BGR2GRAY);
 
-        cap >> frame; // get a new frame from camera
+        vector<uchar> status;
+        Mat err;
 
-        cvtColor(frame, gframe, CV_BGR2GRAY); // color -> gray
+        calcOpticalFlowPyrLK(gframe,next_gframe,pts,next_pts,status,err);//,Size(21,21),3,TermCriteria(TermCriteria::COUNT+TermCriteria::EPS, 30, 0.01),0,1e-4); // compute next positions of features
 
-        // transformation
-        //GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-        //Canny(edges, edges, 0, 30, 3);
+        for(int i = 0; i < next_pts.size(); i++){     // change the color of the found corners
+            color = frame.at<Vec3b>(next_pts[i]); // get color-pixel
+            color[1]=255;
+            color[2]=0;
+            color[3]=0;
+            frame.at<Vec3b>(next_pts[i]) = color; // set color
+        }
 
-        imshow("edges", gframe);
+        imshow("tracking", frame);
 
         if(waitKey(30) >= 0) break;
+
+        gframe = next_gframe.clone();
+        pts = next_pts;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
 
