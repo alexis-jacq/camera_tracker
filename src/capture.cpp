834021de4f3022f2ab4333b7d2689fc491852fdd
Alexis Jacq <alexis.jacq@epfl.ch>
2014-10-15 17:24:22 +0200

    add eyes to detect and track

--- src/capture.cpp
+++ src/capture.cpp
@@ -44,17 +44,23 @@ int main(int, char**)
     Mat gframe; // gray frame that will be used for goodfeaturtotrack
     cap >> frame; // get a new frame from camera
     cvtColor(frame, gframe, CV_BGR2GRAY); // color -> gray
-    vector<Rect> faces; // 2D vector for the list of corner points
+    vector<Rect> faces; // 2D vector for the list of faces
+    vector<Rect> eyes;// 2D vector for the list of eyes
+    // face detect
     face_cascade.detectMultiScale( gframe, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );// detect + display faces
-    vector<Point2f> pts(faces.size());
-    vector<float> height(faces.size());
-    vector<float> width(faces.size());
-    for( int i = 0; i < faces.size(); i++ )
-    {
-        pts[i] = Point( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
-        width[i] = faces[i].width*0.5;
-        height[i] = faces[i].height*0.5;
-    }
+    vector<Point2f> pts1(faces.size());//faces
+    for( size_t i = 0; i < faces.size(); i++ )
+      {
+        pts1[i] = Point( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
+      }
+    // eyes detect
+    eyes_cascade.detectMultiScale( gframe, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+    vector<Point2f> pts2(eyes.size());//eyes
+    for( size_t i = 0; i < eyes.size(); i++ )
+     {
+        pts2[i] = Point( eyes[i].x + eyes[i].width*0.5, eyes[i].y + eyes[i].height*0.5 );
+     }
+
 
 
     // loop with tracking with calcOptimalFlow
@@ -62,8 +68,8 @@ int main(int, char**)
     namedWindow("tracking",1);
 
     Mat next_gframe;
-    vector<Point2f> next_pts;
-
+    vector<Point2f> next_pts1;
+    vector<Point2f> next_pts2;
     while(true)
     {
         cap >> frame;
@@ -72,13 +78,17 @@ int main(int, char**)
         vector<uchar> status;
         Mat err;
 
-        calcOpticalFlowPyrLK(gframe,next_gframe,pts,next_pts,status,err); // compute next positions of features
+        calcOpticalFlowPyrLK(gframe,next_gframe,pts1,next_pts1,status,err); // compute next positions of features
+        calcOpticalFlowPyrLK(gframe,next_gframe,pts2,next_pts2,status,err); // compute next positions of features
 
         // display tracked points
-        for( size_t i = 0; i < pts.size(); i++ )
+        for( size_t i = 0; i < pts1.size(); i++ )
         {
-            ellipse( frame, pts[i], Size( width[i], height[i]), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-            //ellipse( frame, pts[i], Size( 10, 10), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+            ellipse( frame, pts1[i], Size( 50, 50), 0, 0, 360, Scalar( 255, 255, 0 ), 4, 8, 0 );
+        }
+        for( size_t i = 0; i < pts2.size(); i++ )
+        {
+            ellipse( frame, pts2[i], Size( 10, 10), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
         }
 
 
@@ -87,7 +97,8 @@ int main(int, char**)
         if(waitKey(30) >= 0) break;
 
         gframe = next_gframe.clone();
-        pts = next_pts;
+        pts1 = next_pts1;
+        pts2 = next_pts2;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
 
