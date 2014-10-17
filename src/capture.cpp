a4ab24c6876f47d3b84c2c5177969c8c4bd12a58
Alexis Jacq <alexis.jacq@epfl.ch>
2014-10-17 11:32:06 +0200

    detect and manage if new faces appear or desappear

--- src/capture.cpp
+++ src/capture.cpp
@@ -10,7 +10,9 @@ using namespace std;
 /** Functions headers */
 vector<vector<Point2f>> facePointsToTrack(Mat );
 void trackBoxes(Mat , Mat , vector<vector<Point2f>> , vector<vector<Point2f>>& ) ;
-//vector<Point2f> findCenters( vector<vector<Point2f>> ) ;
+vector<Point2f> findCenters( vector<vector<Point2f>> ) ;
+vector<Point2f> findGoodCenters( vector<vector<Point2f>> , vector<Point2f> );
+int findNewPoint(vector<Point2f>, vector<Point2f> );
 
 /** Global variables */
 String face_cascade_name = "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml";
@@ -39,11 +41,13 @@ int main(int, char**)
     cap >> frame; // get a new frame from camera
     cvtColor(frame, gframe, CV_BGR2GRAY); // color -> gray
     vector<vector<Point2f>> boxes = facePointsToTrack(gframe); //
+    auto nb_faces = boxes.size();
 
     // loop : calcOpticalFlow tracking
     //----------------------------------------
     namedWindow("tracking",1);
     Mat next_gframe;
+    int modul = 0;
     while(true)
     {
         cap >> frame;
@@ -57,6 +61,15 @@ int main(int, char**)
             }
         }
 
+        vector<Point2f> centers = findCenters(next_boxes);
+        vector<Point2f> goodCenters = findGoodCenters( next_boxes, centers);
+
+        //for(auto center : centers){
+        //    ellipse( frame, center, Size( 5, 5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+        //}
+        for(auto goodCenter : goodCenters){
+            ellipse( frame, goodCenter, Size( 5, 5), 0, 0, 360, Scalar( 0, 255, 255 ), 4, 8, 0 );
+        }
 
         imshow("tracking", frame);
 
@@ -64,6 +77,38 @@ int main(int, char**)
 
         gframe = next_gframe.clone();
         boxes = next_boxes;
+        centers = goodCenters;
+
+
+        if(modul==100){
+            // check if new face
+            vector<vector<Point2f>> boxesTest = facePointsToTrack(gframe); //
+            vector<Point2f> centersTest = findCenters(boxesTest);
+            auto nb_facesTest = boxesTest.size();
+
+            if(nb_facesTest > nb_faces){
+                cout << "new face detected"<< endl;
+                nb_faces = nb_facesTest;
+                int indice = findNewPoint(centers, centersTest);
+                boxes.push_back(boxesTest[indice]);
+            }
+
+            if(nb_facesTest < nb_faces){
+                cout << "face lost"<< endl;
+                nb_faces = nb_facesTest;
+                int indice = findNewPoint(centersTest, centers);
+                vector<vector<Point2f>> newBoxes;
+                if(indice>0){
+                    for(int i = 0; i<indice; i++) newBoxes.push_back(boxes[i]);
+                    if(indice+1<boxes.size()){
+                        for(int i = indice+1; i<boxes.size(); i++) newBoxes.push_back(boxes[i]);
+                    }
+                }
+                boxes = newBoxes;
+            }
+            modul=0;
+        }
+        else modul++;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
 
@@ -90,7 +135,7 @@ vector<vector<Point2f>> facePointsToTrack(Mat gframe){
 
          // get features in the mask
          vector<Point2f> corners;
-         goodFeaturesToTrack(gframe,corners,50,0.01,10,mask=mask,3,false,0.04); // get the good corners
+         goodFeaturesToTrack(gframe,corners,10,0.01,10,mask=mask,3,false,0.04); // get the good corners
 
          boxes.push_back(corners);
      }
@@ -116,7 +161,7 @@ void trackBoxes(Mat gframe, Mat next_gframe, vector<vector<Point2f>> boxes, vect
 }
 
 
-/*
+
 // find the center of each set of corner in boxes
 //-----------------------------------------------
 vector<Point2f> findCenters( vector<vector<Point2f>> boxes){
@@ -130,18 +175,76 @@ vector<Point2f> findCenters( vector<vector<Point2f>> boxes){
 
         for(auto corner : boxe){
             center = center + corner;
-            auto a = center.x;
-            auto b = center.y;
             //center = corner;
             i=i+1.;
         }
         if(i>0) i = 1./i;
         if(i==0) i = 1.;
         center = center*i;
-        auto a = center.x;
-        auto b = center.y;
         centers.push_back(center);
     }
 
     return centers;
-}*/
+}
+
+
+// find more robust centers
+//-------------------------
+vector<Point2f> findGoodCenters( vector<vector<Point2f>> boxes, vector<Point2f> centers){
+
+    vector<Point2f> goodCenters;
+
+    int index = 0;
+    for(auto boxe : boxes){
+
+        Point2f center = centers[index];
+        Point2f goodCenter(0,0);
+        float i = 0;
+
+        for(auto corner : boxe){
+            float delta = norm(center - corner);
+            float omega = 1./(delta*delta);
+            goodCenter = goodCenter + omega*corner;
+            i=i+omega;
+        }
+        if(i>0) i = 1./i;
+        if(i==0) i = 1.;
+        goodCenter = goodCenter*i;
+        goodCenters.push_back(goodCenter);
+
+        index++;
+    }
+
+    return goodCenters;
+}
+
+// find the indice of the new detected face
+//-----------------------------------------
+int findNewPoint(vector<Point2f> centers, vector<Point2f> tests){
+
+    int index_max = 0;
+    double d_max = 0;
+
+    if(centers.size()>0){
+        for(int i=0; i<tests.size();i++){
+
+            double d_min = 100000;
+            int index_min = 0;
+
+            for(int j=0; j<centers.size();j++){
+
+                if(norm(tests[i] - centers[j]) < d_min ){
+                    d_min = norm(tests[i] - centers[j]);
+                    index_min = j;
+                }
+            }
+            if(norm(tests[i] - centers[index_min]) > d_max ){
+                d_max = norm(tests[i] - centers[index_min]);
+                index_max = i;
+            }
+        }
+    }
+
+    return index_max;
+}
+
